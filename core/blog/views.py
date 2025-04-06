22b2b2c9261c50f53c930b38b280dd7e9a4a05b1
abbasj68 <abbasj.gg@gmail.com>
2025-04-06 12:53:17 +0330

    add view
    list view, create view, edit view, delete view

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView,RedirectView
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,FormView,CreateView,UpdateView,DeleteView
 from .models import Post
 from django.shortcuts import get_object_or_404
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 
 # Create your views here.
 
@@ -41,15 +43,45 @@ class RedirectToMaktab(RedirectView):
         print(post)
         return super().get_redirect_url(**args,**kwargs)
     
-class PostListView(ListView):
-    # queryset = Post.objects.all()
-    model = Post
+class PostListView(LoginRequiredMixin,ListView):
+    queryset = Post.objects.all()
+    # model = Post
     context_object_name = 'posts'
     # paginate_by = 2
-    ordering = 'id'
+    ordering = '-id'
     # def get_queryset(self):
     #     posts = Post.objects.filter(status = True)
     #     return posts     
     
-class PostDetailView(DetailView):
+class PostDetailView(PermissionRequiredMixin,LoginRequiredMixin,DetailView):
         model = Post
+
+''''
+class PostCreateView(FormView):
+    template_name = "contact.html"
+    form_class = PostForm
+    success_url = "/blog/post"
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+'''
+
+class PostCreateView(PermissionRequiredMixin,LoginRequiredMixin,CreateView):
+    model = Post
+    # fields = ['author','title','content','status','category','published_date']
+    form_class = PostForm
+    success_url = '/blog/post'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+    
+class PostEditView(PermissionRequiredMixin,LoginRequiredMixin,UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'   
+    
+class PostDeleteView(PermissionRequiredMixin,LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = '/blog/post/'    
