32ebd8790c30d9f4885c5ef2a8b70ff882d1ae75
abbasj68 <abbasj.gg@gmail.com>
2025-07-12 13:49:40 +0330

    LOCUST

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -105,7 +105,7 @@ from .paginations import DefaultPagination
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    # permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True).order_by("-published_date")
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
